1947df9d53079addc13a2ad443e4b82265f1a90e
unknown <pps7@163.com>
2021-05-21 09:41:55 +0800

    修改 xml py3.2 之后关于 getchildren 的使用问题

--- tools/xmlFunc.py
+++ tools/xmlFunc.py
@@ -1,16 +1,18 @@
 # coding=utf-8
-def printElementByRootNode(node):
+try:
+    from xml.etree.cElementTree import Element
+except ImportError:
+    from xml.etree.ElementTree import Element
+
+
+def printElementByRootNode(node: Element):
     print(node.tag, node.text, node.attrib)
-    _children = node.getchildren()
-    if _children is not None:
-        for _sub in _children:
-            printElementByRootNode(_sub)
+    for _sub in list(node):  # 以前的写法 是 node.getchildren() 但是貌似不用这么写了 ...
+        printElementByRootNode(_sub)
 
 
-def getElementsByTagName(node, tagName: str, outList: list):
-    _children = node.getchildren()
-    if _children is not None:
-        for _sub in _children:
-            if _sub.tag == tagName:
-                outList.append(_sub)
-            getElementsByTagName(_sub, tagName, outList)
+def getElementsByTagName(node: Element, tagName: str, outList: list):
+    for _sub in list(node):  # 以前的写法 是 node.getchildren() 但是貌似不用这么写了 ...
+        if _sub.tag == tagName:
+            outList.append(_sub)
+        getElementsByTagName(_sub, tagName, outList)
